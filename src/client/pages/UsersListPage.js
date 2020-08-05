022f1586c83125fec71312309e250179ca0571fe
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-08-05 18:31:34 +0300

    Dynamic title tags

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -14,13 +14,19 @@ class UsersList extends Component {
     return this.props.users.map((user) => <li key={user.id}>{user.name}</li>)
   }
 
+  head() {
+    return (
+      <Helmet>
+        <title>{`${this.props.users.length} Users App`}</title>
+        <meta property="og:title" content="Users App" />
+      </Helmet>
+    )
+  }
+
   render() {
     return (
       <div>
-        <Helmet>
-          <title>Users App</title>
-          <meta property="og:title" content="Users App" />
-        </Helmet>
+        {this.head()}
         Here's a big list of users
         <ul>{this.renderUsers()}</ul>
       </div>
